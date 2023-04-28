dc4c11c0d52d161eaba35392faf91a501506d022
Ironladd <andredsouza15@gmail.com>
2023-04-28 15:11:31 -0400

    Show loading skellies

--- src/hook/useGames.ts
+++ src/hook/useGames.ts
@@ -25,24 +25,30 @@ const useGames = () => {
 
     const [games, setGames] = useState<Game[]>([]);
     const [error, setError] = useState("");
+    const [isLoading, setLoading] = useState(false);
   
     useEffect(() => {
 
         const controller = new AbortController();
       //need interface to describe the shape of the response
+
+      setLoading(true);
       apiClient
         .get<FetchGameResponse>("/games",{signal: controller.signal})
-        .then((res) => setGames(res.data.results))
+        .then((res) => {setGames(res.data.results)
+        setLoading(false);
+        })
         .catch((err) => {
             if (err instanceof CanceledError) return;
             setError(err.message);//else
+            setLoading(false);
         }); 
 
         //clean up function
         return () => controller.abort();
     },[]);
 
-    return {games,error};
+    return {games,error,isLoading};
 }
 
 export default useGames;
