d627d4a6494b3e3faa9b7e28919b4a4a76d74841
Ironladd <andredsouza15@gmail.com>
2023-04-28 14:39:06 -0400

    Displaying platform icons

--- src/hook/useGames.ts
+++ src/hook/useGames.ts
@@ -6,11 +6,18 @@ interface FetchGameResponse {
     count: number;
     results: Game[];
   }
+export interface Platform {
+    id: number;
+    name: string;
+    slug: string;
+
+}
 
 export interface Game {
     id: number;
     name: string;
     background_image:string;
+    parent_platforms: {platform: Platform}[];
   }
 
 const useGames = () => {
